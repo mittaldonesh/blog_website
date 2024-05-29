acd0fa6f1c68a21865750abb5fa914bd859bc244
donesh mittal <mittaldonesh@gmail.com>
2024-05-29 10:45:03 +0530

    final commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const articleRouter = require("./routes/articles")
+const Article = require('./models/article')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const app = express()
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI);
+  
+app.set("views", "./view")
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.get('/', async(req, res) => {
+    const articles =await Article.find().sort({ createdAt:'desc'})
+    res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(3000)
+
+// To run the code use this one step :   npm run blogwebsite
+                                      
